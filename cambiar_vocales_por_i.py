3f5aea6d32dd477ab15625fc38a47f503ee51b94
Panako14 <ivantorgim@gmail.com>
2018-02-28 11:44:33 +0100

    Reemplazar vocales por i

---
+++ cambiar_vocales_por_i.py
@@ -0,0 +1,6 @@
+
+string_usuario = input("Dime una frase: ")
+
+str_reemplazada = string_usuario.replace("a", "i").replace("e", "i").replace("o", "i").replace("u", "i").replace("A", "I").replace("E", "I").replace("O", "I").replace("U", "I")
+
+print(str_reemplazada)
